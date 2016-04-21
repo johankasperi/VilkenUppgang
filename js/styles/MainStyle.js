764ed396d0299f0d691c815c2e22ed2a1b66f7f6
Johan Kasperi <johan@kasperi.se>
2016-04-22 00:08:13 +0200

    small updates

--- js/styles/MainStyle.js
+++ js/styles/MainStyle.js
@@ -197,6 +197,7 @@ const styles = StyleSheet.create({
   },
   changesContainer: {
     padding: 10,
+    flex: 1,
   },
   journeyDetails: {
     alignItems: 'center',
@@ -241,6 +242,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
+  },
+  map: {
+    flex: 1,
+    height: 150,
+    width: 100,
   },
 });
 
